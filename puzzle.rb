330064d6bc452d73cab879ea6021c25d1a98cd12
Alex Nye <nyebismuth@gmail.com>
2012-10-13 08:13:21 -0700

    merge files, begin puzzle class methods

--- puzzle.rb
+++ puzzle.rb
@@ -4,8 +4,8 @@ class Puzzle
     @immutables = immutables
   end
   
-  def get_available(coord)
-    
+  def get_available(y, x)
+    get_col() & get_row() & get_cell
   end
   
   def get_col
@@ -21,20 +21,40 @@ class Puzzle
   end
 end
 
-def read_puzzle(file_name)
-  def parse_immutables
-    @immutables = Array.new(9).map { |e| e = Array.new(9).map { |f| f = "false" } }
-    # Call after populating values, all non-zero values' coordinates are marked true in immutables
-  end
-end
-
 def solve(puzzle)
   
 end
 
+def show(puzzle)
+  puzzle.each do |line|
+    puts line.join(" ")
+  end
+end
+
 while true
-  print "enter puzzle name or 'exit': "
+  puts "input puzzle name: "
   user_in = gets.chomp
-  if user_in == exit then exit end
-  solve(read_puzzle("./puzzles/#{user_in}.txt"))
+  exit if user_in == 'exit'
+  puzzle = nil
+  
+  File.open(".puzzles/#{user_in}", "r") do |file|
+    line_num = 0
+    puzzle_arr = []
+    immutables = []
+    
+    file.each do |line|
+      this_line_arr = line.scan(/\d/)
+      puzzle_arr.push this_line_arr
+      
+      this_line_arr.each_with_index do |char, char_index|
+        immutables.push [line_num, char_index] if char != "0" 
+      end
+      
+      line_num += 1
+    end
+    
+    puzzle = Puzzle.new(puzzle_arr, immutables) 
+  end
+  
+  solve puzzle
 end
