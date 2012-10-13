4e5840d8c419dd81dddc3877d478e4d6ba98256f
Alex Nye <nyebismuth@gmail.com>
2012-10-13 01:07:38 -0700

    remove .project file

--- puzzle.rb
+++ puzzle.rb
@@ -1,14 +1,40 @@
-class Puzzle
-  @values = []
-  @immutables = Array.new(9).map { |e| e = Array.new(9).map { |f| f = "false" } }
-  
-  def parse_immutables
-    # Call after populating values, all non-zero values' coordinates are marked true in immutables
+class Puzzle 
+  def initialize(values, immutables)
+    @values = values
+    @immutables = immutables
   end
   
+  def get_available(coord)
+    
+  end
+  
+  def get_col
+    
+  end
+  
+  def get_row
+    
+  end
+  
+  def get_cell
+    
+  end
+end
+
+def read_puzzle(file_name)
+  def parse_immutables
+    @immutables = Array.new(9).map { |e| e = Array.new(9).map { |f| f = "false" } }
+    # Call after populating values, all non-zero values' coordinates are marked true in immutables
+  end
+end
+
+def solve(puzzle)
   
 end
 
-class PuzzleReader
-  
+while true
+  print "enter puzzle name or 'exit': "
+  user_in = gets.chomp
+  if user_in == exit then exit end
+  solve(read_puzzle("./puzzles/#{user_in}.txt"))
 end
