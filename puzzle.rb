5344d3dba26f894d777d30972c7be8ebec7d7b53
Alex Nye <nyebismuth@gmail.com>
2012-10-14 22:42:58 -0700

    cell check working, link file reader

--- puzzle.rb
+++ puzzle.rb
@@ -26,11 +26,13 @@ class Puzzle
   end
   
   def get_cell(y, x)
-    (1..9).to_a.map { |digit| digit.to_s }
-  puts(((0..2).each do |n|
-      ((y / 3) * 3) + n end).map do |n|
-        (0..2).map do |m|
-          n = ((x / 3) * 3) + m end end)
+    # for each member of values matrix within the region spanning from the truncated values
+    # of y/3, x/3 to y/3 + 2, x/3 + 2, which form a given 9-box cell of the sudoku puzzle,
+    # read the appropriate value, and subtract the results from a char array of the digits
+    # 0 through 9, thus reporting only the values not already used in the cell
+    
+    # hard to read, I know, but it was fun to make
+    ((1..9).to_a.map { |digit| digit.to_s }) - (@values_matrix[(((y / 3) * 3)..(((y / 3) * 3) + 2))].map { |stuff| stuff[(((x / 3) * 3)..(((x / 3) * 3) + 2))] }).flatten
   end
 end
 
@@ -46,7 +48,7 @@ end
 
 ################ test code
 puzzle = Puzzle.new nil, nil
-puts puzzle.get_cell(1, 9)
+puzzle.get_cell(8, 3)
 
 # while true
   # puts "input puzzle name: "
