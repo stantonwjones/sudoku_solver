60f730b889a7864a88b9697a962f0a71f0012e6a
Alex Nye <nyebismuth@gmail.com>
2012-10-16 21:37:08 -0700

    what a mess

--- solve.rb
+++ solve.rb
@@ -1,44 +1,36 @@
 def get_available(puzzle, y, x) # The workings of this function should be clear as day
-  {:vals => get_col(x) & get_row(y) & get_cell(y, x), :puzzle => puzzle, :y => y, :x => x}
+  puzzle.each { |line| puts line.join(' ') }
+  {:vals => get_col(puzzle, x) & get_row(puzzle, y) & get_cell(puzzle, y, x), :puzzle => puzzle, :y => y, :x => x}
 end
 
-def get_col(x)
+def get_col(puzzle, x)
   (1..9).to_a.map { |digit| digit.to_s } - puzzle.map { |row| row[x] }
 end
 
-def get_row(y)
+def get_row(puzzle, y)
+  puts puzzle[y]
   (1..9).to_a.map { |digit| digit.to_s } - puzzle[y]
 end
 
-def get_cell(y, x) # as you see, quite clear
+def get_cell(puzzle, y, x) # as you see, quite clear
   ((1..9).to_a.map do |digit|
     digit.to_s end) - (puzzle[(((y / 3) * 3)..(((y / 3) * 3) + 2))].map do |col|
       col[(((x / 3) * 3)..(((x / 3) * 3) + 2))] end).flatten
 end
 
-def get_next_coord(moves_hash, steps_ahead = 1)
-  return {:finished => true, :y => moves_hash[:y], :x => moves_hash[:x], :val => moves_hash[:puzzle][y][x]} if moves_hash[:y] == 8 && moves_hash[:x] == 8 && moves_hash[:puzzle][y][x] != "0"
-  return {
-  return {:finished => false, :puzzle => puzzle, :y => moves_hash[:y], :x => moves_hash[:x]} if moves_hash[:puzzle][y][x] != "0" && steps_ahead > 1
-    
-  elsif
-  else
-    get_next_coord(get_available(puzzle, 9 / (moves_hash[:y] + moves_hash[:x] + steps_ahead), (moves_hash[:y] + moves_hash[:x] + steps_ahead) % 9), steps_ahead + 1)
-  end
+def get_next_move(moves_hash, steps_ahead = 1) # do you see?  DO YOU SEE?  http://www.southparkstudios.com/clips/103836/do-you-see
+  # ensure taking at least one step ahead, skipping immutable values, alert if finished
+  return {:finished => true, :puzzle => moves_hash[:puzzle], :val => moves_hash[:puzzle][8][8]} if moves_hash[:y] == 8 && moves_hash[:x] == 8 && moves_hash[:puzzle][8][8] != "0"
+  return {:finished => true, :puzzle => moves_hash[:puzzle], :val => moves_hash[:vals][0]} if moves_hash[:y] == 8 && moves_hash[:x] == 8 && moves_hash[:puzzle][8][8] == "0"
+  return {:finished => false, :puzzle => puzzle, :y => moves_hash[:y], :x => moves_hash[:x]} if moves_hash[:puzzle][moves_hash[:y]][moves_hash[:x]] != "0" && steps_ahead > 1
+
+  get_next_move(get_available(moves_hash[:puzzle], 9 / (moves_hash[:y] + moves_hash[:x] + steps_ahead), (moves_hash[:y] + moves_hash[:x] + steps_ahead) % 9), steps_ahead + 1)
 end
 
-def get_next_move(puzzle, y, x)
-  {:val => , :y => , :x => }
-end
-
-def solve(puzzle_in, y = 0, x = 0)
-  next_move = get_next_move(get_next_coord(get_available(puzzle, y, x)))
+def solve(next_move, y, x)
+  if next_move[:finished] then next_move[:puzzle][8][8] = next_move[:val]; show next_move[:puzzle]; exit end
   
-  show(puzzle, y + x)
-  
-
-  
-  end
+  solve(get_next_move(get_available(puzzle, y, x)))
 end
 
 def show(puzzle, stack_level)
@@ -69,4 +61,4 @@ def get_file
   puzzle_arr
 end
 
-solve(get_next_move(get_next_coord(get_available(get_file, 0, 0))))
+solve(get_next_move(get_available(get_file, 0, 0)), 0, 0)
