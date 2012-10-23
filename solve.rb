1ad1d18662111d218fed7869e76150dbd197a03b
Stanton W. Jones <stantonwjones@gmail.com>
2012-10-23 15:27:19 -0400

    dev version of solve.rb with a bunch of print statements

--- solve.rb
+++ solve.rb
@@ -1,5 +1,13 @@
+require './grid.rb'
 def get_available_guesses(puzzle, y, x)
-  {:vals => get_col(puzzle, x) & get_row(puzzle, y) & get_cell(puzzle, y, x), :puzzle => puzzle, :y => y, :x => x}
+  possibleColVals = get_col(puzzle, x)
+  possibleRowVals = get_row(puzzle, y)
+  possibleCellVals = get_cell(puzzle, y, x)
+  possibleVals = possibleColVals & possibleRowVals & possibleCellVals
+  {:vals => possibleVals,
+   :puzzle => puzzle,
+   :y => y,
+   :x => x}
 end
 
 # Returns the unassigned values for this column of the puzzle
@@ -21,8 +29,9 @@ def get_cell(puzzle, y, x)
 end
 
 # for some reason the coord 0,0 and ONLY 0,0, and for EVERY puzzle produces can't convert nil into array at line 13 in '-'
-def get_next_mutable_box(moves_hash, steps_ahead = 1)
+def get_next_mutable_box( moves_hash, steps_ahead = 1 )
   # ensure taking at least one step ahead, skipping immutable values, alert if finished
+=begin
   print "Hello"
   return {
     :finished => true,
@@ -43,28 +52,73 @@ def get_next_mutable_box(moves_hash, steps_ahead = 1)
     :y => moves_hash[:y], :x => moves_hash[:x]
   } if moves_hash[:puzzle][moves_hash[:y]][moves_hash[:x]] != "0" && steps_ahead > 1
   puts " war."
+=end
 
-  # Error is here?
+  # clone the possible puzzle
+  possiblePuzzle = moves_hash[:puzzle].clone
+  #print "a possible state of the puzzle:\n"
+  #pretty_print possiblePuzzle
+
   # set the current values for x and y
   current_x = moves_hash[:x]
   current_y = moves_hash[:y]
+
+  if possiblePuzzle[current_y][current_x] != "0"
+=begin
+    puts "non-zero puzzle loc at: " +
+        "row, " + current_y.to_s +
+        "; column, " + current_x.to_s
+    puts "the value is: " + possiblePuzzle[current_y][current_x]
+=end
+    moves_hash[:vals] = [possiblePuzzle[current_y][current_x]]
+  else
+=begin
+    puts "the possible values for row " + current_y.to_s + ", col " + current_x.to_s
+    puts moves_hash[:vals].to_s
+=end
+  end
+
+  # Make sure there are possible values for this location
+  return unless moves_hash[:vals] && moves_hash[:vals].length > 0
+
+  # Check for end state
+  if current_x == 8 && current_y == 8
+    possiblePuzzle[current_y][current_x] = moves_hash[:vals][0]
+    puts "\n:::SOLUTION FOUND:::\n"
+    pretty_print( possiblePuzzle )
+    exit
+  end
 
   # get the next value for x and y
   next_x = current_x >=8 ? 0 : current_x + 1
   next_y = current_x >=8 ? current_y + 1 : current_y
 
+  # Call this function recursively for all possible values at this location
+  moves_hash[:vals].each do |possibleVal|
+    nextPuzzleState = possiblePuzzle.clone
+    if nextPuzzleState.class != Grid
+        puts('cloning grid returned an array')
+        exit
+    end
+    nextPuzzleState[current_y][current_x] = possibleVal
+    get_next_mutable_box(get_available_guesses(nextPuzzleState, next_y, next_x), steps_ahead + 1)
+  end
+
+  # If none of the child states are valid, return false
+  return false
+
   # call get_next_mutable_box on the next location going left-right, top-bottom
   # TODO: this is currently just getting the available guesses for each location.  However, we need to add a
   # way to test for a solved sudoku puzzle and exit the program
-  get_next_mutable_box(get_available_guesses(moves_hash[:puzzle], next_y, next_x), steps_ahead + 1)
+  #get_next_mutable_box(get_available_guesses(moves_hash[:puzzle], next_y, next_x), steps_ahead + 1)
 end
 
-def search_for_solution(puzzle, coord)
+def search_for_solution( puzzle, coord )
   #pretty_print(puzzle)
 
 end
 
-def pretty_print(puzzle)
+def pretty_print( puzzle )
   puzzle.each do |line|
     puts line.join(" ")
   end
@@ -72,10 +126,11 @@ end
 
 def get_puzzle_from_file(puzzle_name)
   File.open("./puzzles/#{puzzle_name}", "r") do |file|
-    rows = Array.new(9).map do |element|
+    rows = Grid.new(9)
+    rows.each_index do |index|
         #row = file.gets.scan(/\d*/)
         # Store each row as an array of characters ( strings of length 1 in ruby )
-        file.gets.chomp.split('')
+        rows[index] = file.gets.chomp.split('')
     end
     return rows
   end
@@ -84,4 +139,11 @@ end
 #search_for_solution(get_next_mutable_box(get_available(get_puzzle_from_file, 0, 0)), 0, 0)
 print "input file name: "
 puzzle_from_file = get_puzzle_from_file(gets.chomp)
-(0..8).each do |y| (0..8).each do |x| get_next_mutable_box(get_available_guesses(puzzle_from_file, y, x)) end end
+=begin
+(0..8).each do |y| (0..8).each do |x|
+    get_next_mutable_box( get_available_guesses(puzzle_from_file.clone, y, x) )
+end end
+=end
+get_next_mutable_box( get_available_guesses(puzzle_from_file, 0, 0) )
+puts "No Solution Found for the puzzle"
+exit
